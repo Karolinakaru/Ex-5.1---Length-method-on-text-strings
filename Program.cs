5e32881445b6719ade90a30756eff3c63ed0271f
singhet <singhet@SCSWL4072>
2024-02-06 09:06:04 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string city, voivodeship;
+        int cityLength, voivodeshipLength;
+        Console.WriteLine("Podaj nazwę miasta: ");
+        city = Console.ReadLine();
+
+        Console.WriteLine("Podaj nazwę województwa: ");
+        voivodeship = Console.ReadLine();
+
+        cityLength = city.Length;
+        voivodeshipLength = voivodeship.Length;
+
+        Console.WriteLine($"Nazwa miasta {city}zawiera {cityLength} liter. Nazwa województwa {voivodeship} zawiera {voivodeshipLength} liter.");
+        Console.ReadKey();
+    }
+}
